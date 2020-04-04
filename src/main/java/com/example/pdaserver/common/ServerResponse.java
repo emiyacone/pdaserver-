d1b0363393d64f63569943a857b638205f36dc62
emiyacone <1553642867@qq.com>
2020-04-04 21:55:50 +0800

    '解决jackson的安全隐患'

--- src/main/java/com/example/pdaserver/common/ServerResponse.java
+++ src/main/java/com/example/pdaserver/common/ServerResponse.java
@@ -1,7 +1,8 @@
 package com.example.pdaserver.common;
 
-import org.codehaus.jackson.annotate.JsonIgnore;
-import org.codehaus.jackson.map.annotate.JsonSerialize;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 
 import java.io.Serializable;
 
