5a0e8edc0ae3c4b75ef6f8af3897225946136e10
emiyacone <1553642867@qq.com>
2020-04-06 21:18:45 +0800

    'pagehelper修改'

--- src/test/java/com/example/pdaserver/PdaserverApplicationTests.java
+++ src/test/java/com/example/pdaserver/PdaserverApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.pdaserver;
 
 import com.example.pdaserver.dao.OrderinfoMapper;
 import com.example.pdaserver.dao.ProdetailMapper;
+import com.example.pdaserver.service.OrderService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,10 +10,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PdaserverApplicationTests {
     @Autowired
-    private OrderinfoMapper orderinfoMapper;
+    private OrderService orderService;
     @Test
     void contextLoads() {
-        System.out.println(orderinfoMapper.selectlistbyFacNoAndOrerNoAndBacketNoAndProductNoAndProName(null,"%8%",null,null,null).size());
+        System.out.println(orderService.getOrderList(1,2).getData());
     }
 
 }
